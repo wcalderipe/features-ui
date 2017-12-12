76ece16db0631e781753d192f4fe5d629129f5d2
William Calderipe <wcalder@thoughtworks.com>
2017-12-11 23:22:11 -0300

    Add ApplicationFeatures component

---
+++ test/unit/components/ApplicationFeaturesTest.js
@@ -0,0 +1,45 @@
+import sinon from 'sinon'
+import {React, shallow} from './testSetup'
+
+import * as client from '../../../src/clients/features'
+import ApplicationFeatures from '../../../src/components/ApplicationFeatures'
+
+describe('ApplicationFeatures component', () => {
+  const sandbox = sinon.sandbox.create() 
+  const props = {
+    applicationId: 1
+  }
+  const expectedFeatures = [
+    {id: 1, name: 'feature01'},
+    {id: 2, name: 'feature02'}
+  ]
+  const fetchPromise = Promise.resolve({data: expectedFeatures})
+
+  beforeEach(() => {
+    sandbox.stub(client, 'fetch').returns(fetchPromise)
+  })
+
+  afterEach(() => {
+    sandbox.restore()
+  })
+
+  it('fetches features by application id after mount', async () => {
+    const wrapper = shallow(<ApplicationFeatures {...props} />)
+
+    await fetchPromise
+
+    expect(wrapper.state()).toHaveProperty('features')
+    wrapper.update()	
+    expect(wrapper.state('features')).toEqual(expectedFeatures)
+  })
+
+  it('renders a row for each feature', async () => {
+    const wrapper = shallow(<ApplicationFeatures {...props} />)
+
+    await fetchPromise
+    wrapper.update()	
+
+    expect(wrapper.find('tbody').children().length).toEqual(2)
+  })
+})
+
