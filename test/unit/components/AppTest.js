a2591a8cf2bfafbb566625a64051d5bd9c6c415a
William Calderipe <wcalder@thoughtworks.com>
2017-12-13 20:35:56 -0300

    Assert App route component name

--- test/unit/components/AppTest.js
+++ test/unit/components/AppTest.js
@@ -9,24 +9,28 @@ describe('App component', () => {
     const route = wrapper.find('Switch').childAt(0)
 
     expect(route.prop('path')).toEqual('/')
+    expect(route.prop('component').name).toEqual('Applications')
   })
 
   it('adds application page route', () => {
     const route = wrapper.find('Switch').childAt(1)
 
     expect(route.prop('path')).toEqual('/applications/:applicationId')
+    expect(route.prop('component').name).toEqual('Application')
   })
 
   it('adds feature page route', () => {
     const route = wrapper.find('Switch').childAt(2)
 
     expect(route.prop('path')).toEqual('/features/:featureId')
+    expect(route.prop('component').name).toEqual('Feature')
   })
 
   it('adds parameter form route', () => {
     const route = wrapper.find('Switch').childAt(3)
 
     expect(route.prop('path')).toEqual('/features/:featureId/parameters/new')
+    expect(route.prop('component').name).toEqual('ParameterForm')
   })
 })
 
